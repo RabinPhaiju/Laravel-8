172cef152758c52d819e33dfde6af51ca6d5f65f
RabinPhaiju <rabinphaiju15@gmail.com>
2021-01-23 14:26:15 +0545

    Trigger Events

--- livewire_blog/app/Http/Livewire/Find.php
+++ livewire_blog/app/Http/Livewire/Find.php
@@ -11,12 +11,33 @@ class Find extends Component
     public $num2;
     public $result;
 
+    public $message='';
+    // if event is from full page then it must protected, and other inline class can access it.
+    // This function is called when the event=resultcalcualted is triggered.
+    protected $listeners = ['resultcalculated'=>'calculatedResult'];
+
+    public function calculatedResult(){
+        if($this->num1>$this->num2){
+            $this->message='Num1 is greater than num2';
+        }else if($this->num1<$this->num2){
+        $this->message='Num2 is greater than num1';
+        }else{
+            $this->message ='Num1 is equal to num2';
+        }
+    }
+
     public function add(){
         $this->result = $this->num1 + $this->num2;
+    }
+    public function subtract(){
+        $this->result = $this->num1 - $this->num2;
+        // method 2 to call event
+        $this->emit('resultcalculated');
     }
     
     public $student = ['s_name'=>''];
 
+    // mount is used to empty the defined variable.
     public function mount(){
         $this->name='';
     }
--- livewire_blog/resources/views/livewire/find.blade.php
+++ livewire_blog/resources/views/livewire/find.blade.php
@@ -20,4 +20,25 @@
             <br>
         <button wire:click='add'>Show Result</button>
     </div>
+    <div>
+        <form wire:submit.prevent="add">
+            <input type="number" name="" wire:model.laxy='num1'>
+            <input type="number" name="" wire:model.laxy='num2'>
+            {{-- method 1 to call event --}}
+            <button type="submit" wire:click="$emit('resultcalculated')">Submit</button>
+        </form>
+            Result : {{$result}}
+            <br>
+            {{$message}}
+    </div>
+    <div>
+        <form wire:submit.prevent="subtract">
+            <input type="number" name="" wire:model.laxy='num1'>
+            <input type="number" name="" wire:model.laxy='num2'>
+            <button type="submit">Submit</button>
+        </form>
+            Result : {{$result}}
+            <br>
+            {{$message}}
+    </div>
 </div>
