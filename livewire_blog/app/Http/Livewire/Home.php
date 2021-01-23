216693fc58a99ba4eded9a6cc60d52000669dc17
RabinPhaiju <rabinphaiju15@gmail.com>
2021-01-23 14:48:09 +0545

    Nesting components

--- livewire_blog/app/Http/Livewire/Home.php
+++ livewire_blog/app/Http/Livewire/Home.php
@@ -7,6 +7,28 @@ use Livewire\Component;
 class Home extends Component
 {
     public $name;
+    public $num1;
+    public $num2;
+    public $result;
+    public $message = '';
+
+    protected $listeners = ['resultcalculated'=>'calculatedResult'];
+
+    public function add(){
+        $this->result = $this->num1 + $this->num2;
+        $this->emit('resultcalculated');
+    }
+
+    public function calculatedResult(){
+        if($this->num1>$this->num2){
+            $this->message='Num1 is greater than num2';
+        }else if($this->num1<$this->num2){
+        $this->message='Num2 is greater than num1';
+        }else{
+            $this->message ='Num1 is equal to num2';
+        }
+    }
+
     public function mount($name){
         $this->name=$name;
     }
--- livewire_blog/resources/views/livewire/home.blade.php
+++ livewire_blog/resources/views/livewire/home.blade.php
@@ -1,3 +1,15 @@
 <div>
     Home page of {{$name}}
+    @livewire('counter')
+    @livewire('counter')
+    <div>
+        <form wire:submit.prevent="add">
+            <input type="number" name="" wire:model.laxy='num1'>
+            <input type="number" name="" wire:model.laxy='num2'>
+            <button type="submit">Submit</button>
+        </form>
+            Result : {{$result}}
+            <br>
+            {{$message}}
+    </div>
 </div>
