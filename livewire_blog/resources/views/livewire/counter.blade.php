ef9c16185842dc06529f3a9126ac016f04c29961
RabinPhaiju <rabinphaiju15@gmail.com>
2021-01-24 17:29:36 +0545

    files deleted

--- livewire_blog/resources/views/livewire/counter.blade.php
+++
@@ -1,7 +0,0 @@
-<div>
-    <h1>Hello {{$data}} from livewire counter</h1>
-    <div style="text-align: center">
-        <button wire:click="increment">Increment</button>
-        <h1>{{ $count }}</h1>
-    </div>
-</div>
--- livewire_blog/resources/views/livewire/help.blade.php
+++
@@ -1,4 +0,0 @@
-<div>
-    <h1>Topic : {{$topic}}</h1>
-    Success is as dangerous as failure. 
-</div>
