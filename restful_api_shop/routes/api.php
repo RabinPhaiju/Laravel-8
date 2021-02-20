d7fc21af5ebaee206d595e1214fb3280bf358990
RabinPhaiju <rabinphaiju15@gmail.com>
2021-02-20 15:08:29 +0545

    cors

--- restful_api_shop/routes/api.php
+++ restful_api_shop/routes/api.php
@@ -38,7 +38,7 @@ use App\Http\Controllers\UserController;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:api/*')->get('/user', function (Request $request) {
     return $request->user();
 });
 
